7b58b5541def8a4c5d02b91b0aa67bdfe57cd01c
Risto <risto.kekovic@logeecom.com>
2021-06-29 15:46:05 +0200

    Update post composer script
    ISSUE: PLCS26-50

--- Script/Core.php
+++ Script/Core.php
@@ -28,7 +28,7 @@ class Core
     private static function copyDirectory($src, $dst)
     {
         $dir = opendir($src);
-        @mkdir($dst);
+        @mkdir($dst, 0777, true);
         while (false !== ($file = readdir($dir))) {
             if (($file !== '.') && ($file !== '..')) {
                 if (is_dir($src . '/' . $file)) {
